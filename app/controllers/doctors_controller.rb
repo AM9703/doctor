e16c344b717ee2cffbda24c68152a5d943157da9
Aman Bhargav <amanbhargav.ror@gmail.com>
2022-03-15 12:39:32 +0530

    Correct indentation.

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,25 +1,25 @@
 class DoctorsController < ApplicationController
 
-	def index
-		@doctor = Doctor.all
-	end
+  def index
+    @doctor = Doctor.all
+  end
 
-	def new
-		@doctor = Doctor.new
-	end
+  def new
+    @doctor = Doctor.new
+  end
 
-	def create
-		@doctor = Doctor.new(doctor_param)
-		if @doctor.save
-			redirect_to doctor_path
-		else
-			
-		end
-	end
+  def create
+    @doctor = Doctor.new(doctor_param)
+    if @doctor.save
+      redirect_to doctor_path
+    else
+      
+    end
+  end
 
-	private
+  private
 
-	  def doctor_param
-	   params.require(:doctor).permit(:name, :specialization, :fees, :start_time, :end_time)
-	  end
+    def doctor_param
+     params.require(:doctor).permit(:name, :specialization, :fees, :start_time, :end_time)
+    end
 end
